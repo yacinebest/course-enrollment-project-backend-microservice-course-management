c658b8de1bf7e5f977fdd17a330e70b1dbb8e141
yacinebest <yacine.besta@gmail.com>
2021-06-29 09:57:13 +0100

    Init Project.

---
+++ src/main/java/com/besttocode/microservicecoursemanagement/MicroserviceCourseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.besttocode.microservicecoursemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCourseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCourseManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/besttocode/microservicecoursemanagement/MicroserviceCourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.besttocode.microservicecoursemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCourseManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
